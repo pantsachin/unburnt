a2ae97b1b965d1f3b78921329dab3b1425c7f927
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2021-06-29 18:32:42 +0530

    fix: link color

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -13,7 +13,10 @@ const Contact = ({ title, subtitle, id }) => {
           <div className="input-area">
             {" "}
             <div className="input-area button-area">
-              <a style={{textDecoration: "none"}} href="https://docs.google.com/forms/d/e/1FAIpQLSd8t9R4-vFtDhxDFrM0y14wmANYwfZuxhmyAduZgf6fMNQkDw/viewform">
+              <a
+                style={{ textDecoration: "none", color: "inherit" }}
+                href="https://docs.google.com/forms/d/e/1FAIpQLSd8t9R4-vFtDhxDFrM0y14wmANYwfZuxhmyAduZgf6fMNQkDw/viewform"
+              >
                 {" "}
                 <Button
                   label="Send Contact Form"
